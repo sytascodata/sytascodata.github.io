941aa412de233f7a2be5cfca9a3d41537286d9fc
saiteja501 <30906665+saiteja501@users.noreply.github.com>
2024-01-29 20:20:48 +0530

    Update Files

---
+++ JS/script.js
@@ -0,0 +1,60 @@
+function visible(partial) {
+    var $t = partial,
+        $w = jQuery(window),
+        viewTop = $w.scrollTop(),
+        viewBottom = viewTop + $w.height(),
+        _top = $t.offset().top,
+        _bottom = _top + $t.height(),
+        compareTop = partial === true ? _bottom : _top,
+        compareBottom = partial === true ? _top : _bottom;
+
+    return ((compareBottom <= viewBottom) && (compareTop >= viewTop) && $t.is(':visible'));
+
+}
+
+$(window).scroll(function(){
+
+  if(visible($('.count-digit')))
+    {
+      if($('.count-digit').hasClass('counter-loaded')) return;
+      $('.count-digit').addClass('counter-loaded');
+      
+$('.count-digit').each(function () {
+  var $this = $(this);
+  jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
+    duration: 5000,
+    easing: 'swing',
+    step: function () {
+      $this.text(Math.ceil(this.Counter));
+    }
+  });
+});
+    }
+})
+
+
+// slider
+
+$(document).ready(function(){
+    $('.customer-logos').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});
+    
